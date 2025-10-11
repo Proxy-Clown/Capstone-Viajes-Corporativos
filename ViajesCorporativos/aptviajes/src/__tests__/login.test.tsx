a232da2fdfde7472fb451f1ab99ef9b7b5d876d9
vca07 <javier.ac.contactos@gmail.com>
2025-10-10 23:26:01 -0300

    Add:Pruebas Unitarias con Jest

---
+++ ViajesCorporativos/aptviajes/src/__tests__/login.test.tsx
@@ -0,0 +1,8 @@
+import "@testing-library/jest-dom"
+import {render,screen} from "@testing-library/react"
+import NeoTravelFlowLogin from "../app/login/page"
+
+//TEST DE PRUEBA
+describe('page', ()=>{
+    it('renders input')
+})
