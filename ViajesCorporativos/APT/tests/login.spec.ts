46c1fd79694c6c50700a62709308c379a263432f
ChrisTdawe <christianriquelme2018@gmail.com>
2025-10-18 21:45:46 -0300

    pruebas playwright 2 navegator

--- ViajesCorporativos/APT/tests/login.spec.ts
+++ ViajesCorporativos/APT/tests/login.spec.ts
@@ -7,7 +7,7 @@ test.describe('Login NeoTravelFlow', () => {
 
   // Se ejecuta antes de cada test
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:3000');
+    await page.goto('http://localhost:3000/login');
   });
 
   test('Pasar por login y redirige a home', async ({ page }) => {
@@ -17,11 +17,16 @@ test.describe('Login NeoTravelFlow', () => {
 
     // Hacer clic en el botón de login
     await page.getByRole('button', { name: 'Ingresar Sesion' }).click();
-
+    await page.waitForLoadState('networkidle') 
     // Esperar explícitamente a que la URL cambie a /Home
-    await page.waitForURL('**/Home', { timeout: 8000 });
+    //await page.waitForURL('http://localhost:3000/Home', { timeout: 800000 });
+    
     await expect(page.getByRole('heading', { name: 'Acciones rápidas' }))
-    .toBeVisible({ timeout: 8000 });
+    .toBeVisible({timeout: 15000});
   });
 
 });
+//Firefox = Pasan
+//WTF
+//Chromium = Pasan
+//Webit 
